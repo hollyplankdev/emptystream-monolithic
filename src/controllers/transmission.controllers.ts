973dcf091da88c54475f9494421720e693b16b08
HollyGrahamPlank <hollygrahamplank@gmail.com>
2023-12-14 17:19:31 -0600

    Testing business logic

--- src/controllers/transmission.controllers.ts
+++ src/controllers/transmission.controllers.ts
@@ -1,12 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { RequestHandler } from "express";
+import { Transmission } from "../models/transmission.js";
 
 const create: RequestHandler = (req, res) => {
   throw new Error("Not implemented!");
 };
 
-const read: RequestHandler = (req, res) => {
-  throw new Error("Not implemented!");
+const read: RequestHandler = async (req, res) => {
+  const transmission = await Transmission.findById(req.params.id);
+  res.status(200).json({
+    message: "OK",
+    transmission,
+  });
 };
 
 const update: RequestHandler = (req, res) => {
--- src/models/transmission.ts
+++ src/models/transmission.ts
@@ -1,15 +1,11 @@
 import { Schema, model } from "mongoose";
 
 export interface ITransmission {
-  /** The ID of this transmission. */
-  id: String;
-
   /** The name describing this transmission. */
   name: String;
 }
 
 export const TransmissionSchema = new Schema<ITransmission>({
-  id: { type: String, required: true },
   name: { type: String, required: true },
 });
 
