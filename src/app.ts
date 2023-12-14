4ff9ce3f10de4cd2aebbab99d80830c82a153396
HollyGrahamPlank <hollygrahamplank@gmail.com>
2023-12-14 14:46:11 -0600

    Server works without .env

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,31 @@
-console.log("Hello World!");
+import * as dotenv from "dotenv";
+import express from "express";
+
+//
+//  DEFAULTS
+//
+
+const DEFAULT_EXPRESS_PORT = 3000;
+
+//
+//  APPLICATION START
+//
+
+// Load env vars from the `.env` file
+dotenv.config();
+
+// Load our port either from env vars, or use the default
+const expressPort = process.env.EXPRESS_PORT || DEFAULT_EXPRESS_PORT;
+
+// Construct the Express application
+const app = express();
+
+// Setup a default router
+app.get("/", (req, res) => {
+  res.send(JSON.stringify({ message: "Hello World!" }));
+});
+
+// ...Start the Express server!
+app.listen(expressPort, () => {
+  console.log(`Listening on port ${expressPort}`);
+});
