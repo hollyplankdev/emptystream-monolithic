31e162a852b3dc794e82b8c09375739b939ac069
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-01-26 14:17:53 -0600

    Wrote flexible disk storage structure

---
+++ src/utils/disk_storage_provider.ts
@@ -0,0 +1,19 @@
+import { Readable, Writable } from "stream";
+
+export default interface DiskStorageProvider {
+  /**
+   * Create a readable stream to a specific file located in this storage provider.
+   *
+   * @param key The key to the file. Depending on the storage provider, this could be an ID, a file
+   *   path, or some other unique identifier.
+   */
+  createReadStream(key: string): Promise<Readable>;
+
+  /**
+   * Create a writable stream to a specific file located in this storage provider.
+   *
+   * @param key The key to the file. Depending on the storage provider, this could be an ID, a file
+   *   path, or some other unique identifier.
+   */
+  createWriteStream(key: string): Promise<Writable>;
+}
---
+++ src/utils/transmission_storage.ts
@@ -0,0 +1,46 @@
+import * as path from "path";
+import { Writable, Readable } from "stream";
+import DiskStorageProvider from "./disk_storage_provider";
+
+/** Values that Transmission stems types are allowed to be */
+export type StemType = ["source", "drums", "bass", "vocals", "other"];
+
+export default class TransmissionStorage {
+  private storage: DiskStorageProvider;
+
+  private fileExtension: string = ".mp3";
+
+  //
+  //  Init
+  //
+
+  constructor(storageProvider: DiskStorageProvider) {
+    this.storage = storageProvider;
+  }
+
+  //
+  //  Public Methods
+  //
+
+  public async createStemWriteStream(transmissionId: string, type: StemType): Promise<Writable> {
+    return this.storage.createWriteStream(this.createFileKey(transmissionId, type));
+  }
+
+  public async createStemReadStream(transmissionId: string, type: StemType): Promise<Readable> {
+    return this.storage.createReadStream(this.createFileKey(transmissionId, type));
+  }
+
+  //
+  //  Private Methods
+  //
+
+  /**
+   * Creates a file key string as a path in the format of: "transmissions/id/type.mp3"
+   *
+   * @param transmissionId The ID of the transmission to look in
+   * @param type The specific type of stem to address
+   */
+  private createFileKey(transmissionId: string, type: StemType): string {
+    return path.join("transmissions", transmissionId, `${type}${this.fileExtension}`);
+  }
+}
