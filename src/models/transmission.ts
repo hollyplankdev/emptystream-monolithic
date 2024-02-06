1ed252c0c47041fc0101481f59ef911c37fbe647
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-02-06 16:12:04 -0600

    Schema accounts for API changes

--- src/models/transmission.ts
+++ src/models/transmission.ts
@@ -1,8 +1,11 @@
 import { Schema, model } from "mongoose";
 
-export interface ITransmission {
-  /** The name describing this transmission. */
-  name: String;
+export interface ISplitOperation {
+  /** The current status of the operation. */
+  status: String;
+
+  /** How complete the operation is, from 0-100. */
+  percentage: number;
 }
 
 export interface ITimestamps {
@@ -10,9 +13,27 @@ export interface ITimestamps {
   updatedAt?: Date;
 }
 
+export interface ITransmission {
+  /** The name describing this transmission. */
+  name: String;
+
+  /** The stem types that this transmission has. */
+  stems: [String];
+
+  /** Information about the audio splitting operation for this Transmission. */
+  splitOperation: ISplitOperation;
+}
+
+export const SplitOperationSchema = new Schema<ISplitOperation>({
+  status: { type: String, required: true },
+  percentage: { type: Number, required: true },
+});
+
 export const TransmissionSchema = new Schema<ITransmission>(
   {
     name: { type: String, required: true },
+    stems: { type: [String], required: true },
+    splitOperation: { type: SplitOperationSchema, required: true },
   },
   { timestamps: true },
 );
