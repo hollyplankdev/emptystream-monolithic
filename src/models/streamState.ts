3f25f00725a8e59e068eed68c5412d982a1dfd0b
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-02-23 14:05:01 -0600

    Created StreamState schema

---
+++ src/models/streamState.ts
@@ -0,0 +1,58 @@
+import mongoose, { ObjectId, Schema, model } from "mongoose";
+import { ITimestamps } from "./timestamps.js";
+
+export interface IChannelTuning {
+  /** The index of the channel that this tuning represents. */
+  index: number;
+
+  /** Details about the transmission that this channel is tuned to. */
+  transmission: {
+    /** The ID of the transmission that this channel is tuned to. */
+    id: ObjectId;
+
+    /** The stem of the transmission that this channel is tuned to. */
+    stem: string;
+  };
+
+  /** When this tuning started. */
+  startTime?: Date;
+
+  /**
+   * How long it should take to go from some previous tuning settings to the tuning settings
+   * described in this object.
+   */
+  transitionDuration?: Date;
+}
+
+export interface IStreamState {
+  /** The tunings for each channel on the stream. */
+  tunings: [IChannelTuning];
+}
+
+export const ChannelTuningSchema = new Schema<IChannelTuning>(
+  {
+    index: { type: Number, required: true },
+    transmission: {
+      type: new Schema<IChannelTuning["transmission"]>(
+        {
+          id: { type: mongoose.Types.ObjectId, required: true },
+          stem: { type: String, required: true },
+        },
+        { _id: false },
+      ),
+      required: true,
+    },
+    startTime: { type: Date, required: false },
+    transitionDuration: { type: Date, required: false },
+  },
+  { _id: false },
+);
+
+export const StreamStateSchema = new Schema<IStreamState>(
+  {
+    tunings: { type: [ChannelTuningSchema], required: true },
+  },
+  { timestamps: true },
+);
+
+export const StreamState = model<IStreamState & ITimestamps>("StreamState", StreamStateSchema);
