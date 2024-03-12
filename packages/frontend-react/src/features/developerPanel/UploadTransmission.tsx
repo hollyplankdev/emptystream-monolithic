e16dd27df40144fee3efe47326216ce27c628b91
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-03-12 18:00:18 -0500

    Transmission form UI

--- packages/frontend-react/src/features/developerPanel/UploadTransmission.tsx
+++ packages/frontend-react/src/features/developerPanel/UploadTransmission.tsx
@@ -1,46 +1,104 @@
-import { Group, Text } from "@mantine/core";
-import { Dropzone } from "@mantine/dropzone";
-import { IconUpload, IconPhoto, IconX } from "@tabler/icons-react";
+import { Box, Button, Group, Text, TextInput } from "@mantine/core";
+import { Dropzone, FileWithPath } from "@mantine/dropzone";
+import { useForm } from "@mantine/form";
+import { IconMusic, IconPhoto, IconUpload, IconX } from "@tabler/icons-react";
+import isURLSafe from "../../utils/isURLSafe";
+
+interface FormValues {
+  file: File | null;
+  name: string;
+}
 
 export default function UploadTransmission() {
-  const onGoodFiles = (files) => {
-    // TODO
-    console.log(files);
+  const form = useForm<FormValues>({
+    initialValues: {
+      file: null,
+      name: "",
+    },
+    validate: {
+      // Make sure that the file field is set correctly
+      file: (value) => (value !== null ? null : "No file selected"),
+      name: (value) => (isURLSafe(value) ? null : "Name isn't URL safe"),
+    },
+  });
+
+  const onGoodFiles = (files: FileWithPath[]) => {
+    form.clearFieldError("file");
+    form.setFieldValue("file", files[0]);
+
+    if (files[0].path) {
+      form.setFieldValue("name", files[0].name.replace(".mp3", ""));
+    } else {
+      form.setFieldValue("name", "");
+    }
   };
 
-  const onBadFiles = (files) => {
-    // TODO
-    console.log(files);
+  const onBadFiles = () => {
+    form.setFieldError("file", "Invalid file selected");
+    form.setFieldValue("name", "");
   };
+
+  const onFormSubmit = form.onSubmit(() => {
+    // TODO
+    console.log("TODO");
+  });
 
   return (
-    <>
-      <Dropzone onDrop={onGoodFiles} onReject={onBadFiles} accept={{ "audio/mpeg": [".mp3  "] }}>
-        <Group justify="center" gap="x1" mih={80} style={{ pointerEvents: "none" }}>
-          {/* When the hovered file is valid */}
-          <Dropzone.Accept>
-            <IconUpload />
-          </Dropzone.Accept>
+    <Box maw={340} mx="auto">
+      <form onSubmit={onFormSubmit}>
+        {/* The input allowing an audio file to be selected */}
+        <Dropzone
+          onDrop={onGoodFiles}
+          onReject={onBadFiles}
+          multiple={false}
+          accept={{ "audio/mpeg": [".mp3  "] }}
+        >
+          <Group justify="center" gap="x1" mih={80} style={{ pointerEvents: "none" }}>
+            {/* When the hovered file is valid */}
+            <Dropzone.Accept>
+              <IconUpload />
+            </Dropzone.Accept>
 
-          {/* When the hovered file is invalid */}
-          <Dropzone.Reject>
-            <IconX />
-          </Dropzone.Reject>
+            {/* When the hovered file is invalid */}
+            <Dropzone.Reject>
+              <IconX />
+            </Dropzone.Reject>
 
-          {/* When nothing is happening */}
-          <Dropzone.Idle>
-            <IconPhoto />
-          </Dropzone.Idle>
+            {/* When nothing is happening */}
+            <Dropzone.Idle>
+              {form.values.file !== null ? <IconMusic /> : <IconPhoto />}
+            </Dropzone.Idle>
 
-          <div>
-            <Text size="x1" inline>
-              Click to upload MP3 audio.
-            </Text>
-            <Text size="sm">Only one file can be uploaded at a time!</Text>
-          </div>
+            <div>
+              <Text size="x1" inline>
+                {form.values.file !== null ? form.values.file.name : "Click to upload MP3 audio."}
+              </Text>
+              <Text size="sm">
+                {form.values.file === null ? "Only one file can be uploaded at a time!" : ""}
+              </Text>
+            </div>
+          </Group>
+        </Dropzone>
+
+        {/* Display errors with the selected file, if there are any. */}
+        {form.errors.file && (
+          <Text c="red" mt={5} size="xs">
+            {form.errors.file}
+          </Text>
+        )}
+
+        {/* The input allowing the name of the transmission to be set */}
+        <TextInput
+          label="Transmission Name"
+          placeholder="somethingcool"
+          disabled={form.values.file === null}
+          {...form.getInputProps("name")}
+        />
+        <Group justify="end" mt="md">
+          <Button type="submit">Submit</Button>
         </Group>
-      </Dropzone>
-      <Text>huh.</Text>
-    </>
+        <Text>huh.</Text>
+      </form>
+    </Box>
   );
 }
