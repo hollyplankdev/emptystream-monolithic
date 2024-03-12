e04b290940dbf908caf76b9cca8381625394cc76
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-03-12 14:08:16 -0500

    Formatted frontend

--- packages/frontend-react/src/features/diceRoll/diceRollSlice.ts
+++ packages/frontend-react/src/features/diceRoll/diceRollSlice.ts
@@ -1,27 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export interface DiceRollState {
-    /**
-     * The current dice roll.
-     */
-    value: number;
+  /** The current dice roll. */
+  value: number;
 }
 
 const initialState: DiceRollState = {
-    value: 0
+  value: 0,
 };
 
 export const diceRollSlice = createSlice({
-    name: "diceRoll",
-    initialState,
-    reducers: create => ({
-        roll: create.reducer(state => {
-            state.value = Math.floor(Math.random() * 21)
-        })
+  name: "diceRoll",
+  initialState,
+  reducers: (create) => ({
+    roll: create.reducer((state) => {
+      state.value = Math.floor(Math.random() * 21);
     }),
-    selectors: {
-        selectValue: state => state.value,
-    },
+  }),
+  selectors: {
+    selectValue: (state) => state.value,
+  },
 });
 
 export const { roll } = diceRollSlice.actions;
