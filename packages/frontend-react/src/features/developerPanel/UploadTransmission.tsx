c3ab0b71d13859e0642bbed6c99c9fe3a3df5123
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-03-12 16:58:18 -0500

    Used tutorial to polish upload box

--- packages/frontend-react/src/features/developerPanel/UploadTransmission.tsx
+++ packages/frontend-react/src/features/developerPanel/UploadTransmission.tsx
@@ -1,36 +1,44 @@
-import { Text } from "@mantine/core";
+import { Group, Text } from "@mantine/core";
 import { Dropzone } from "@mantine/dropzone";
 import { IconUpload, IconPhoto, IconX } from "@tabler/icons-react";
 
 export default function UploadTransmission() {
-  const onGivenFiles = (files) => {
+  const onGoodFiles = (files) => {
+    // TODO
+    console.log(files);
+  };
+
+  const onBadFiles = (files) => {
+    // TODO
     console.log(files);
   };
 
   return (
     <>
-      <Dropzone onDrop={onGivenFiles} accept={{ "audio/mpeg": [".mp3  "] }}>
-        {/* When the hovered file is valid */}
-        <Dropzone.Accept>
-          <IconUpload />
-        </Dropzone.Accept>
+      <Dropzone onDrop={onGoodFiles} onReject={onBadFiles} accept={{ "audio/mpeg": [".mp3  "] }}>
+        <Group justify="center" gap="x1" mih={80} style={{ pointerEvents: "none" }}>
+          {/* When the hovered file is valid */}
+          <Dropzone.Accept>
+            <IconUpload />
+          </Dropzone.Accept>
 
-        {/* When the hovered file is invalid */}
-        <Dropzone.Reject>
-          <IconX />
-        </Dropzone.Reject>
+          {/* When the hovered file is invalid */}
+          <Dropzone.Reject>
+            <IconX />
+          </Dropzone.Reject>
 
-        {/* When nothing is happening */}
-        <Dropzone.Idle>
-          <IconPhoto />
-        </Dropzone.Idle>
+          {/* When nothing is happening */}
+          <Dropzone.Idle>
+            <IconPhoto />
+          </Dropzone.Idle>
 
-        <div>
-          <Text size="x1" inline>
-            Click to upload MP3 audio.
-          </Text>
-          <Text size="sm">Only one file can be uploaded at a time!</Text>
-        </div>
+          <div>
+            <Text size="x1" inline>
+              Click to upload MP3 audio.
+            </Text>
+            <Text size="sm">Only one file can be uploaded at a time!</Text>
+          </div>
+        </Group>
       </Dropzone>
       <Text>huh.</Text>
     </>
