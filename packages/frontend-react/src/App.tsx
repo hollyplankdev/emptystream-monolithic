eed2ff6af76dc951d27e7ce56759befb362f0c16
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-03-25 16:17:12 -0500

    Added ReactQuery

--- packages/frontend-react/src/App.tsx
+++ packages/frontend-react/src/App.tsx
@@ -2,6 +2,7 @@ import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import "@mantine/dropzone/styles.css";
 import { ModalsProvider } from "@mantine/modals";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import DeveloperPanel from "./features/developerPanel/DeveloperPanel";
 import StreamImage from "./features/streamImage/StreamImage";
@@ -10,28 +11,32 @@ import { TransmissionAudio } from "./features/transmissionAudio/TransmissionAudi
 import VolumeSlider from "./features/volumeSlider/VolumeSlider";
 import theme from "./theme";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <MantineProvider theme={theme} defaultColorScheme="dark">
       <ModalsProvider>
-        <div className="App">
-          <DeveloperPanel />
-          <div className="AppContainer">
-            <div className="CenterPanel">
-              <StreamImage />
-              <VolumeSlider />
-              <StreamTuner index={0} />
-              <StreamTuner index={1} />
-              <StreamTuner index={2} />
-              <StreamTuner index={3} />
-              <StreamTuner index={-3} holographic />
-              <TransmissionAudio index={0} />
-              <TransmissionAudio index={1} />
-              <TransmissionAudio index={2} />
-              <TransmissionAudio index={3} />
+        <QueryClientProvider client={queryClient}>
+          <div className="App">
+            <DeveloperPanel />
+            <div className="AppContainer">
+              <div className="CenterPanel">
+                <StreamImage />
+                <VolumeSlider />
+                <StreamTuner index={0} />
+                <StreamTuner index={1} />
+                <StreamTuner index={2} />
+                <StreamTuner index={3} />
+                <StreamTuner index={-3} holographic />
+                <TransmissionAudio index={0} />
+                <TransmissionAudio index={1} />
+                <TransmissionAudio index={2} />
+                <TransmissionAudio index={3} />
+              </div>
             </div>
           </div>
-        </div>
+        </QueryClientProvider>
       </ModalsProvider>
     </MantineProvider>
   );
