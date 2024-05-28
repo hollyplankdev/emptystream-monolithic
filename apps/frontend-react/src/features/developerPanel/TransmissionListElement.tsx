5d4ea2b63288c4963666db96b5e801eb2dc3ce06
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-05-28 17:12:16 -0500

    List element uses skeletons

--- apps/frontend-react/src/features/developerPanel/TransmissionListElement.tsx
+++ apps/frontend-react/src/features/developerPanel/TransmissionListElement.tsx
@@ -1,9 +1,19 @@
 import { DbObject, ITransmission } from "@emptystream/shared";
-import { ActionIcon, Group, Loader, Paper, Progress, Stack, Text } from "@mantine/core";
+import {
+  ActionIcon,
+  Group,
+  Loader,
+  Modal,
+  Paper,
+  Progress,
+  Skeleton,
+  Stack,
+  Text,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { IconDots, IconExclamationCircle, IconMusic } from "@tabler/icons-react";
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
-import TransmissionAPI from "../../api/TransmissionAPI";
+import useQueryTransmission from "../../queries/useQueryTransmission";
+import TransmissionDetails from "./TransmissionDetails";
 import styles from "./TransmissionListElement.module.css";
 
 export interface TransmissionListElementProps {
@@ -14,7 +24,9 @@ export interface TransmissionListElementProps {
 //  Sub-Elements
 //
 
-function StemIcon({ transmission }: { transmission: ITransmission }) {
+function StemIcon({ transmission }: { transmission?: ITransmission }) {
+  if (!transmission) return <Skeleton height={20} circle />;
+
   switch (transmission.splitOperation.status) {
     case "complete":
       return <IconMusic size={20} />;
@@ -27,9 +39,14 @@ function StemIcon({ transmission }: { transmission: ITransmission }) {
   }
 }
 
-function TransmissionProgress({ transmission }: { transmission: ITransmission }) {
+function TransmissionName({ transmission }: { transmission?: ITransmission }) {
+  if (!transmission) return <Skeleton height={8} />;
+  return <Text size="md">{transmission.name}</Text>;
+}
+
+function TransmissionProgress({ transmission }: { transmission?: ITransmission }) {
   // If we're already done splitting, exit early
-  if (transmission.splitOperation.status === "complete") return undefined;
+  if (!transmission || transmission.splitOperation.status === "complete") return undefined;
 
   const isError = transmission.splitOperation.status === "failed";
   const color = isError ? "red" : "blue";
@@ -44,36 +61,29 @@ function TransmissionProgress({ transmission }: { transmission: ITransmission })
   );
 }
 
-//
-//  Sub-Hooks
-//
+function TransmissionDetailsMenu({
+  transmission,
+  isOpened,
+  openFunc,
+  closeFunc,
+}: {
+  transmission?: ITransmission;
+  isOpened: boolean;
+  openFunc: () => void;
+  closeFunc: () => void;
+}) {
+  if (!transmission) return undefined;
 
-function useTransmission(initialData: DbObject<ITransmission>) {
-  // Setup - how often should we refetch?
-  const [refetchInterval, setRefetchInterval] = useState<number | false>(false);
-
-  const query = useQuery({
-    queryKey: ["transmission", "id", initialData._id],
-    queryFn: () => TransmissionAPI.get(initialData._id),
-    staleTime: 1000 * 60,
-    refetchInterval,
-    initialData,
-  });
-
-  switch (query.data.splitOperation.status) {
-    /** If this query is not loading, mark that it shouldn't auto refresh */
-    case "complete":
-    case "failed":
-      if (refetchInterval !== false) setRefetchInterval(false);
-      break;
-
-    /** If this query is still loading, mark that it should refresh every so often. */
-    default:
-      if (refetchInterval !== 1000 * 1) setRefetchInterval(1000 * 1);
-      break;
-  }
-
-  return query.data;
+  return (
+    <>
+      <ActionIcon onClick={openFunc} variant="transparent" color="white" size="md">
+        <IconDots size="100%" stroke={2} />
+      </ActionIcon>
+      <Modal opened={isOpened} onClose={closeFunc} title={`Transmission: "${transmission.name}"`}>
+        <TransmissionDetails />
+      </Modal>
+    </>
+  );
 }
 
 //
@@ -81,19 +91,23 @@ function useTransmission(initialData: DbObject<ITransmission>) {
 //
 
 export default function TransmissionListElement({ initialData }: TransmissionListElementProps) {
-  const transmission = useTransmission(initialData);
+  const transmission = useQueryTransmission({ id: initialData._id, initialData });
+  const [detailsOpened, { open: openDetails, close: closeDetails }] = useDisclosure(false);
 
   return (
     <Paper withBorder radius={0} pl="xs" pr="xs" pt={1} pb={1} className={styles.elementButton}>
       <Group justify="flex-start">
         <StemIcon transmission={transmission} />
         <Stack gap={0} flex={1} justify="center" mih="35px">
-          <Text size="md">{transmission.name}</Text>
+          <TransmissionName transmission={transmission} />
           <TransmissionProgress transmission={transmission} />
         </Stack>
-        <ActionIcon variant="transparent" color="white" size="md">
-          <IconDots size="100%" stroke={2} />
-        </ActionIcon>
+        <TransmissionDetailsMenu
+          transmission={transmission}
+          isOpened={detailsOpened}
+          openFunc={openDetails}
+          closeFunc={closeDetails}
+        />
       </Group>
     </Paper>
   );
